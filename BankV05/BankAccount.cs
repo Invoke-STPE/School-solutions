a80722aebcb605c8fa965c8ae75b827ace10feb2
Fustyv <47424622+Fustyv@users.noreply.github.com>
2020-09-16 16:26:13 +0200

    Add BankV05 solution

---
+++ BankV05/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿namespace BankV05
+{
+    /// <summary>
+    /// This class represents a very simple bank account.
+    /// Only the amount of money on the account is represented.
+    /// </summary>
+    public class BankAccount
+    {
+        private double _balance;
+        private string _name;
+
+        public BankAccount(string name)
+        {
+            _balance = 0.0;
+            _name = name;
+        }
+
+        public double Balance
+        {
+            get { return _balance;}
+        }
+
+        public string GetName{
+             get { return _name;}
+        }
+
+        public void Deposit(double amount)
+        {
+            _balance = _balance + amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            _balance = _balance - amount;
+        }
+    }
+}
