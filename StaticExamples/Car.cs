381e3773e9fec5956d6904200fb296044c01b8f7
Fustyv <47424622+Fustyv@users.noreply.github.com>
2020-09-17 20:39:48 +0200

    Opgave 1.6b, 1.7
    Tilføjet StockPortfolio og StaticExamples

---
+++ StaticExamples/Car.cs
@@ -0,0 +1,57 @@
+﻿namespace StaticExamples
+{
+    /// <summary>
+    /// A very simple representation of a car
+    /// </summary>
+    public class Car
+    {
+        #region Instance fields
+        private string _licensePlate;
+        private int _price;
+        static private int _carObjects;
+        static private int _numOfLicensePlateUsage;
+        static private int _numOfPriceUsage;
+        #endregion
+
+        #region Constructor
+        public Car(string licensePlate, int price)
+        {
+            _licensePlate = licensePlate;
+            _price = price;
+            _carObjects += 1;
+        }
+        #endregion
+
+        #region Properties
+        public string LicensePlate
+        {
+            get { 
+                _numOfLicensePlateUsage += 1;
+                return _licensePlate; 
+            }
+            set {
+                _numOfLicensePlateUsage += 1;
+                _licensePlate = value; 
+                }
+        }
+
+        public int Price
+        {
+            get { 
+                _numOfPriceUsage += 1; 
+                return _price; 
+            }
+            set { 
+                _numOfPriceUsage += 1;
+                _price = value; 
+            }
+        } 
+        #endregion
+
+        #region Methods
+        public static string PrintUsageStatistics() {
+            return $"{_carObjects} Car objects have been created!\nLicense Plate properties have been used {_numOfLicensePlateUsage} times\n Price Property have been used {_numOfPriceUsage} times.";
+        }
+        #endregion
+    }
+}
---
+++ StaticExamples/InsertCodeHere.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StaticExamples
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            #region ListMethods test
+            List<int> aList = new List<int> { 23, 86, 51, 11, 39 };
+            ListMethods listMethodsObject = new ListMethods();
+
+            int smallest = ListMethods.FindSmallestNumber(aList);
+            Console.WriteLine($"The smallest number in the list is : {smallest}");
+
+            int average = ListMethods.FindAverage(aList);
+            Console.WriteLine($"The average of the list is : {average}"); 
+            #endregion
+
+            #region CarObject test
+            Car car1 = new Car("AAAA", 50);
+            car1.LicensePlate = "AAAAAAA";
+            car1.LicensePlate = "AAAAAAA";
+            car1.LicensePlate = "AAAAAAA";
+            car1.LicensePlate = "AAAAAAA";
+            car1.Price = 100;
+            Car car2 = new Car("BBBB", 5000);
+            car2.Price = 100;
+            car2.Price = 100;
+            System.Console.WriteLine(Car.PrintUsageStatistics());
+            #endregion
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
